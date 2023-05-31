ef6a2c4db6f0d56b1f462bb293ac6f4fa68bb38b
Btkm2 <beketmuratbek57@gmail.com>
2023-05-31 13:56:34 +0600

    Initial Commit

---
+++ Tugan Zher/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Tugan Zher
+//
+//  Created by Beket Muratbek on 31.05.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tugan Zher/Tugan_ZherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tugan_ZherApp.swift
+//  Tugan Zher
+//
+//  Created by Beket Muratbek on 31.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Tugan_ZherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
