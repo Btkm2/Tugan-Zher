7b9bd86f1306a9c2cebc5842984c51fcb1982aff
Btkm2 <beketmuratbek57@gmail.com>
2023-06-08 15:56:42 +0600

    Added new things

--- Tugan Zher/Views/AccountPageView.swift
+++ Tugan Zher/Views/AccountPageView.swift
@@ -9,42 +9,87 @@ import SwiftUI
 
 struct AccountPageView: View {
     var body: some View {
-        ScrollView {
-            VStack {
+        NavigationView {
+            ScrollView {
                 VStack {
-                    Image(systemName: "person")
-                        .resizable()
-                        .renderingMode(.original)
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 30, height: 30)
-                        .overlay {
-                            Circle()
-                                .stroke(.black, lineWidth: 2)
-                                .foregroundColor(Color.white)
-                                .frame(width: 60, height: 60)
+                    VStack {
+                        Image(systemName: "person")
+                            .resizable()
+                            .renderingMode(.original)
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 30, height: 30)
+                            .overlay {
+                                Circle()
+                                    .stroke(.black, lineWidth: 2)
+                                    .foregroundColor(Color.white)
+                                    .frame(width: 60, height: 60)
+                            }
+                            .padding(.bottom, 15)
+                        Text("Muratbekuly Beket")
+                            .font(.system(size: 18, weight: .bold, design: .default))
+                            .foregroundColor(Color.white)
+                        Text("1st level user")
+                            .font(.system(size: 14, weight: .medium, design: .default))
+                            .foregroundColor(Color.yellow)
+                    }
+                    .frame(width: UIScreen.main.bounds.width, height: 250)
+                    //                .border(Color.red)
+                    .background(Color("LightGreen"))
+                    VStack {
+                        NavigationLink {
+                            
+                        } label: {
+                            AccountListElements(image_name: "person.circle.fill", text: "Account management", padding: 150)
                         }
-                        .padding(.bottom, 15)
-                    Text("Muratbekuly Beket")
-                        .font(.system(size: 18, weight: .bold, design: .default))
-                        .foregroundColor(Color.white)
-                    Text("1st level user")
-                        .font(.system(size: 14, weight: .medium, design: .default))
-                        .foregroundColor(Color.yellow)
+                        NavigationLink {
+                            
+                        } label: {
+                            AccountListElements(image_name: "wallet.pass", text: "Payment", padding: 255)
+                        }
+                        NavigationLink {
+                            
+                        } label: {
+                            AccountListElements(image_name: "suit.heart", text: "Saved", padding: 275)
+                        }
+                        NavigationLink {
+                            
+                        } label: {
+                            AccountListElements(image_name: "hand.thumbsup", text: "Reviews", padding: 255)
+                        }
+                        NavigationLink {
+                            
+                        } label: {
+                            AccountListElements(image_name: "list.bullet.rectangle.portrait", text: "Questions", padding: 235)
+                                .foregroundColor(Color.black)
+                        }
+                        NavigationLink {
+                            
+                        } label: {
+                            AccountListElements(image_name: "questionmark.circle", text: "Contact us", padding: 230)
+                        }
+                        NavigationLink {
+                            
+                        } label: {
+                            AccountListElements(image_name: "hand.raised.square", text: "Security Q&A", padding: 210)
+                        }
+                        NavigationLink {
+                            
+                        } label: {
+                            AccountListElements(image_name: "percent", text: "Special offers", padding: 210)
+                        }
+                        NavigationLink {
+                            
+                        } label: {
+                            AccountListElements(image_name: "network", text: "Travellers network", padding: 230)
+                        }
+                    }
+                    .foregroundColor(Color.black)
                 }
-                .frame(width: UIScreen.main.bounds.width, height: 250)
-//                .border(Color.red)
-                .background(Color("LightGreen"))
-                AccountListElements(image_name: "person.circle.fill", text: "Account management", padding: 150)
-                AccountListElements(image_name: "wallet.pass", text: "Payment", padding: 255)
-                AccountListElements(image_name: "suit.heart", text: "Saved", padding: 275)
-                AccountListElements(image_name: "hand.thumbsup", text: "Reviews", padding: 255)
-                AccountListElements(image_name: "list.bullet.rectangle.portrait", text: "Questions", padding: 235)
-                AccountListElements(image_name: "questionmark.circle", text: "Contact us", padding: 230)
-                AccountListElements(image_name: "hand.raised.square", text: "Security Q&A", padding: 210)
-                AccountListElements(image_name: "percent", text: "Special offers", padding: 210)
-                AccountListElements(image_name: "network", text: "Travellers network", padding: 230)
             }
+            .navigationBarHidden(true)
+            .navigationTitle("")
         }
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
